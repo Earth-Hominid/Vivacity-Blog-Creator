ac09aeebdb00483f5b484bc83e0fd7cfb5740190
C <earthhominid@gmail.com>
2022-05-14 18:08:41 -0300

    added async handler to routes

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,34 +1,36 @@
+asyncHandler = require('express-async-handler');
+
 // @Desc Get blogs
 // @route GET /api/blogs
 // @access is Private
 
-const getBlogs = async (req, res) => {
+const getBlogs = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get Blogs!' });
-};
+});
 
 // @desc Create blog
 // @route POST /api/blogs
 // @access Private
 
-const createBlog = async (req, res) => {
+const createBlog = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Create blog' });
-};
+});
 
 // @desc Update blog post
 // @route PUT /api/blogs/:id
 // @access Private
 
-const updateBlog = async (req, res) => {
+const updateBlog = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Blog ${req.params.id}` });
-};
+});
 
 // @desc Delete blog post
 // @ route DELETE /api/blogs/:id
 // @access Private
 
-const deleteBlog = async (req, res) => {
+const deleteBlog = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Blog ${req.params.id}` });
-};
+});
 
 module.exports = {
   getBlogs,
