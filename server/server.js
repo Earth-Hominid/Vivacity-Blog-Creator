ff4d45d48f741cafe426d98677588b9133600ee8
C <earthhominid@gmail.com>
2022-05-14 16:46:18 -0300

    error handler middlware added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const errorHandler = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 8001;
 
 const app = express();
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/blogs', require('./routes/blogRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
