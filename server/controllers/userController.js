ee87339372e5d650bce2980f6288562226faa574
C <earthhominid@gmail.com>
2022-05-19 20:39:22 -0300

    added user login logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -36,7 +36,7 @@ const registerUser = asyncHandler(async (req, res) => {
   if (user) {
     res.status(201).json({
       _id: user.id,
-      name: `first_name + ' ' + last_name`,
+      name: first_name + ' ' + last_name,
       email: user.email,
       alias: user.alias,
     });
@@ -51,7 +51,22 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access Public
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'Login User' });
+  const { email, password } = req.body;
+
+  // Search if user email exists in db.
+  const user = await User.findOne({ email });
+
+  // check the password, password in db is hashed
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials');
+  }
 });
 
 // @desc Get user data
