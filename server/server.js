f07b82de604e98b3823ba99d9b4bf1d8dbf82441
C <earthhominid@gmail.com>
2022-05-19 17:59:20 -0300

    user controller logic and route logic added

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/blogs', require('./routes/blogRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
