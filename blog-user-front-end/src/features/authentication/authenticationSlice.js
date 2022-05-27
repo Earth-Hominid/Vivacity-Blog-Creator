3f5fe9bf58fc0610cbbe3ee104822247358e0237
C <earthhominid@gmail.com>
2022-05-27 17:46:43 -0300

    logic added for error message

--- blog-user-front-end/src/features/authentication/authenticationSlice.js
+++ blog-user-front-end/src/features/authentication/authenticationSlice.js
@@ -1,4 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import authenticationService from './authenticationService';
 
 // Get user from local storage with JWT. Local storage only stores strings so parse first.
 
@@ -11,6 +13,23 @@ const initialState = {
   isLoading: false,
   message: '',
 };
+
+export const register = createAsyncThunk(
+  'authentication/register',
+  async (user, thunkAPI) => {
+    try {
+      return await authenticationService.register(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const authenticationSlice = createSlice({
   name: 'authentication',
