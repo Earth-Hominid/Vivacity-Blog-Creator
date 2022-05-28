7061082bd162d56180c69b80205543121f9048f3
C <earthhominid@gmail.com>
2022-05-28 00:01:55 -0300

    added sign out feature

--- blog-user-front-end/src/components/navigation/Navigation.js
+++ blog-user-front-end/src/components/navigation/Navigation.js
@@ -1,8 +1,18 @@
 import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../../assets/logo/light-logo.png';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../../features/auth/authSlice';
 
 const Navigation = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const logoutUser = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/');
+  };
 
   return (
     <nav className="h-12 whitespace-nowrap flex flex-row items-center sticky top-0 z-50 bg-[#FFFEFD]">
@@ -12,10 +22,12 @@ const Navigation = () => {
         </Link>
       </div>
       <div className="flex flex-row align-items justify-end w-full space-x-6 mr-10">
-        <div>
-          <Link to="/login">
-            <button
-              className="
+        {user ? (
+          <>
+            <div>
+              <button
+                onClick={logoutUser}
+                className="
               font-extrabold
               font-[Montserrat]
             bg-sky-500 
@@ -30,21 +42,48 @@ const Navigation = () => {
             hover:border-sky-500
             rounded-md 
             text-base"
-            >
-              Login
-            </button>
-          </Link>
-        </div>
-        <div>
-          <Link to="/register">
-            <p
-              className="font-[Montserrat] py-1 
+              >
+                Logout
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div>
+              <Link to="/login">
+                <button
+                  className="
+              font-extrabold
+              font-[Montserrat]
+            bg-sky-500 
+            py-1
+            px-2
+            border-2
+            border-sky-500
+            text-white 
+            hover:border-2 
+            hover:bg-[#FFFEFD] 
+            hover:text-sky-500 
+            hover:border-sky-500
+            rounded-md 
+            text-base"
+                >
+                  Login
+                </button>
+              </Link>
+            </div>
+            <div>
+              <Link to="/register">
+                <p
+                  className="font-[Montserrat] py-1 
             px-2 font-bold text-base hover:text-sky-500"
-            >
-              Sign up
-            </p>
-          </Link>
-        </div>
+                >
+                  Sign up
+                </p>
+              </Link>
+            </div>{' '}
+          </>
+        )}
       </div>
     </nav>
   );
