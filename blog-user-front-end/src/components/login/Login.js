237cf37e4a9e2388c44da28e3e0f85de90ef63ee
C <earthhominid@gmail.com>
2022-05-28 00:37:56 -0300

    login logic added

--- blog-user-front-end/src/components/login/Login.js
+++ blog-user-front-end/src/components/login/Login.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { login, reset } from '../../features/auth/authSlice';
+import LoadingSpinner from '../loading-spinner/LoadingSpinner';
 
 const Login = () => {
   const [registerFormData, setRegisterFormData] = useState({
@@ -8,6 +12,25 @@ const Login = () => {
   });
 
   const { email, password } = registerFormData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate('/dashboard');
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setRegisterFormData((prevState) => ({
@@ -18,7 +41,17 @@ const Login = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
   };
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <>
