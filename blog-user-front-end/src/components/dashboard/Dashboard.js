a955b893d3d366386b59157fb08473650dd6c6eb
C <earthhominid@gmail.com>
2022-05-30 21:11:09 -0300

    updated styling

--- blog-user-front-end/src/components/dashboard/Dashboard.js
+++ blog-user-front-end/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Welcome from './Welcome';
 import CreateBlogModal from './modals/CreateBlogModal';
 import LoadingSpinner from '../loading-spinner/LoadingSpinner';
+import { getBlogs, reset } from '../../features/blogs/blogSlice';
 
 const Dashboard = () => {
   const [initialPage, setInitialPage] = useState(true);
@@ -22,12 +23,29 @@ const Dashboard = () => {
   const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
+  const { blogs, isLoading, isError, message } = useSelector(
+    (state) => state.blogs
+  );
 
   useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+
     if (!user) {
       navigate('/vivacity');
     }
-  }, [user, navigate]);
+
+    dispatch(getBlogs());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <>
