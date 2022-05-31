3e605c6851765f2ec22d04f6a8b927685d8f80d4
C <earthhominid@gmail.com>
2022-05-30 21:13:03 -0300

    added logic to save blog form inputs and get blogs from db

--- blog-user-front-end/src/features/blogs/blogService.js
+++ blog-user-front-end/src/features/blogs/blogService.js
@@ -4,13 +4,32 @@ import axios from 'axios';
 
 const API_URL = '/api/blogs/';
 
-const createBlog = async (blogData) => {
-  const response = await axios.post(API_URL, blogData);
+// Create new blog
+const createBlog = async (blogData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-  // axios puts data inside an object called data
-  if (response.data) {
-    localStorage.setItem('blog', JSON.stringify(response.data));
-  }
+  const response = await axios.post(API_URL, blogData, config);
 
   return response.data;
 };
+
+// Get user blogs
+const getBlogs = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+};
+
+const blogService = { createBlog, getBlogs };
+
+export default blogService;
--- blog-user-front-end/src/features/blogs/blogSlice.js
+++ blog-user-front-end/src/features/blogs/blogSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import blogService from './blogService';
 
 const user = JSON.parse(localStorage.getItem('user'));
 
@@ -7,13 +8,80 @@ const initialState = {
   isError: false,
   isSuccess: false,
   isLoading: false,
+  message: '',
 };
+
+// Create new blog
+export const createBlog = createAsyncThunk(
+  'blogs/create',
+  async (blogData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await blogService.createBlog(blogData, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const getBlogs = createAsyncThunk(
+  'blogs/getAll',
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await blogService.getBlogs(token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const blogSlice = createSlice({
   name: 'blog',
   initialState,
   reducers: {
     reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createBlog.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createBlog.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.blogs.push(action.payload);
+      })
+      .addCase(createBlog.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getBlogs.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getBlogs.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.blogs = action.payloads;
+      })
+      .addCase(getBlogs.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
   },
 });
 
