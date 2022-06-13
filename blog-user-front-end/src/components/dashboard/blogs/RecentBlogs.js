e90555e0f838d342279e77871ddb28f4332caa4c
C <earthhominid@gmail.com>
2022-06-13 14:15:56 -0300

    recent blog logic for UI

--- blog-user-front-end/src/components/dashboard/blogs/RecentBlogs.js
+++ blog-user-front-end/src/components/dashboard/blogs/RecentBlogs.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import BlogEntry from './BlogEntry';
 
 const RecentBlogs = ({ blogs }) => {
-  return <h2>{blogs}</h2>;
+  return (
+    <>
+      <section
+        id="recent__blogs"
+        className=" m-4 flex items-center justify-center md:justify-start"
+      >
+        {blogs.length > 0 ? (
+          <>
+            <div className="flex flex-col md:flex-row md:space-x-6">
+              {blogs.map((blog) => (
+                <BlogEntry key={blog.id} blog={blog} />
+              ))}
+            </div>
+          </>
+        ) : (
+          <h2 className="text-lg text-gray-400 font-semibold md:text-xl">
+            I am currently an empty space, newly created blogs will appear here!
+          </h2>
+        )}
+      </section>
+    </>
+  );
 };
 
 export default RecentBlogs;
