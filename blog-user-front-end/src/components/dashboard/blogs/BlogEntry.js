7111ba46155d9c72ec505a96eeb1d2b6cd8f9d4a
C <earthhominid@gmail.com>
2022-06-14 22:31:02 -0300

    update logic

--- blog-user-front-end/src/components/dashboard/blogs/BlogEntry.js
+++ blog-user-front-end/src/components/dashboard/blogs/BlogEntry.js
@@ -1,43 +1,73 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { deleteBlog, updateBlog } from '../../../features/blogs/blogSlice';
+import { deleteBlog } from '../../../features/blogs/blogSlice';
+import UpdateBlogForm from '../form/UpdateBlogForm';
 
-const BlogEntry = ({ blog, openUpdateModal }) => {
+const BlogEntry = ({ blog }) => {
+  const [updateModal, setUpdateModal] = useState(false);
+
   const dispatch = useDispatch();
 
   const deleteBlogEntry = () => {
     return dispatch(deleteBlog(blog._id));
   };
 
-  const updateBlogEntry = () => {
-    return dispatch(updateBlog(blog._id));
+  const openUpdateModal = () => {
+    setUpdateModal(true);
+  };
+
+  const closeUpdateModal = () => {
+    setUpdateModal(false);
   };
 
   return (
     <div
       id="blog"
-      className="
-      m-2 md:m-6 h-auto w-[25rem] md:w-[15rem] md:min-h-[10rem] flex flex-col bg-white px-4 py-1 rounded-lg border-slate-300 border-2 hover:border-indigo-600 hover:border-dotted hover:shadow-lg hover:bg-indigo-50"
+      className="m-2 md:m-6 h-auto w-[25rem] md:w-[35rem] lg:w-[45rem] flex flex-col bg-white px-4 py-1 rounded-lg border-slate-300 border-2 hover:border-indigo-600 hover:border-dotted hover:shadow-lg hover:bg-indigo-50"
     >
       <div className="w-full flex flex-row items-center justify-end my-2">
-        <button
-          onClick={openUpdateModal}
-          className="hover:bg-indigo-200 mr-2 rounded-lg p-1"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 md:w-6 md:h-6 text-indigo-500 hover:text-indigo-700"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
+        {updateModal ? (
+          <button
+            onClick={closeUpdateModal}
+            className="hover:bg-rose-200 mr-2 rounded-lg p-1"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-            />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 md:w-6 md:h-6 text-rose-500 hover:text-rose-700"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>{' '}
+          </button>
+        ) : (
+          <button
+            onClick={openUpdateModal}
+            className="hover:bg-indigo-200 mr-2 rounded-lg p-1"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 md:w-6 md:h-6 text-indigo-500 hover:text-indigo-700"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+              />
+            </svg>{' '}
+          </button>
+        )}
+
         <button
           onClick={deleteBlogEntry}
           className="rounded-lg hover:bg-rose-200 p-1"
@@ -62,8 +92,9 @@ const BlogEntry = ({ blog, openUpdateModal }) => {
         {blog.title}
       </h2>
       <h4 className="text-xs md:text-sm font-bold text-slate-400 mt-2 pb-6">
-        {new Date(blog.createdAt).toLocaleString('en-US')}
+        {new Date(blog.updatedAt).toLocaleString('en-US')}
       </h4>
+      {updateModal && <UpdateBlogForm blog={blog} />}
     </div>
   );
 };
