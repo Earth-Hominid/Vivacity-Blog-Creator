8d2c9ed1a9f7845171cac3b4b48fe8c87bd1103c
C <earthhominid@gmail.com>
2022-06-14 22:30:19 -0300

    added title prop to button

--- blog-user-front-end/src/components/buttons/SaveButton.js
+++ blog-user-front-end/src/components/buttons/SaveButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const SaveButton = ({ onSubmit }) => {
+const SaveButton = ({ onSubmit, title }) => {
   return (
     <button
       type="submit"
       onSubmit={onSubmit}
       className="bg-indgo-500 rounded-lg border-2 bg-sky-500 text-white border-sky-500 hover:text-sky-500 font-bold px-8 py-2 mb-5 lg:px-14 lg:py-3 hover:bg-white"
     >
-      Save Blog
+      {title}
     </button>
   );
 };
