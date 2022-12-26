3b0a364b2a0d5609b767b50215c6887b58d2e0fc
C <earthhominid@gmail.com>
2022-12-26 20:57:45 -0300

    yarn build

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8001;
 
 connectDatabase();
 const app = express();
-app.use(helmet());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
