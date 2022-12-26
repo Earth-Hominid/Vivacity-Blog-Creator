db7aa9fa9f811d7e82a291bf7d8d48af4db0d412
C <earthhominid@gmail.com>
2022-12-26 18:36:49 -0300

    deployment

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
 
 app.use('/api/blogs', require('./routes/blogRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
