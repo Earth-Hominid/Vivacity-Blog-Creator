f01c17df4389420d01da79bd6d9273854a8f44ed
C <earthhominid@gmail.com>
2022-12-27 09:28:41 -0300

    client

--- server/server.js
+++ server/server.js
@@ -16,11 +16,11 @@ app.use('/api/users', require('./routes/userRoutes'));
 
 // Serve frontend routes
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build/static')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) =>
     res.sendFile('index.html', {
-      root: path.join(__dirname, '../client/build/'),
+      root: path.join(__dirname + '/client/build/index.html'),
     })
   );
 } else {
