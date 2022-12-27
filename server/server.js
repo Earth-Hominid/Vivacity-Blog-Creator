e8700fa57c5223ab65634e87999e619405f6a0c9
C <earthhominid@gmail.com>
2022-12-26 21:19:11 -0300

    switched from build to public

--- server/server.js
+++ server/server.js
@@ -17,13 +17,7 @@ app.use('/api/users', require('./routes/userRoutes'));
 
 // Serve frontend routes
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
-    )
-  );
+  app.use(express.static(path.join(__dirname, '../client/public')));
 } else {
   app.get('/', (req, res) => res.send('Set to production'));
 }
